f48b8dfea6004137a8cb03eba1f76846c904ab2d
matteodev10 <119347739+matteodev10@users.noreply.github.com>
2024-06-06 23:59:45 +0200

    add db properties and connection

--- src/main/java/com/datacoffee/tournament/TournamentApplication.java
+++ src/main/java/com/datacoffee/tournament/TournamentApplication.java
@@ -8,10 +8,12 @@ import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients
 /*@ComponentScans({ @ComponentScan("com.datacoffee.tournaments.controller") })
 @EnableJpaRepositories("com.datacoffee.tournaments.repository")
